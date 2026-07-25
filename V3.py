cb15d744a9075a6920e9d49142dcf47094411c6c
ahmadaaaz <109924199+ahmadaaaz@users.noreply.github.com>
2026-07-25 23:46:43 +0300

    Update V3.py

--- V3.py
+++ V3.py
@@ -14,7 +14,8 @@ st.title("🚀 Simple Paste & Send Mailer")
 
 # --- STEP 1: PASTE DATA ---
 st.warning("Mail column's title must be " '"Email"')
-st.warning("### :red[USING THE CODE LOCALLY IS SUPER DUPER LOOPER IMPORTANT SINCE STREAMLIT SERVERS ARENT STABLE AND MAY REFRETH THE PAGE AFTER 30-40 MAILS]")
+with st.container(border=True):
+    st.text("### :red[USING THE CODE LOCALLY IS SUPER DUPER LOOPER IMPORTANT SINCE STREAMLIT SERVERS ARENT STABLE AND MAY REFRETH THE PAGE AFTER 30-40 MAILS]")
 st.subheader("1. Paste Data From Google Sheets")
 raw_pasted_data = st.text_area("Copy your rows (including headers) and paste here:", height=250)
 
